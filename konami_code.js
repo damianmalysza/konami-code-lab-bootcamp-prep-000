1df9dc866ebac80097b0d96305ed97f2f3d8509b
Damian Malysza <damian.malysza@gmail.com>
2020-09-22 02:35:03 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,26 +11,20 @@ const codes = [
   "a"
 ];
 
-
+var index=0
 
 function init() {
   document.body.addEventListener("keydown",konamiCodeChecker)
 }
 
-let index=0
-
 function konamiCodeChecker(e){
-  const key=e.key
-  console.log(key)
-  if(key===codes[index]){
-    index++
-    console.log(index)
-  } if (index===codes.length){
-    alert("Hurray!")
-    console.log("index=codes triggered")
-    index=0
-  } else {
-    index=0
-    console.log("else triggered")
-  }
+ if (e.key===codes[index]){
+   index++
+   if (index===codes.length){
+     alert("Hurray!")
+     index=0
+   }
+ } else{
+   index=0
+ }
 }
